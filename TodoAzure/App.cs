f8cfc27222d02c664e31465d95db35cadacc047f
Joseph Sewell <jsewell@preemptive.com>
2018-04-17 11:00:53 -0400

    Add app-disabling code to TodoAzure.App
    This code manages a property in local device storage which indicates if
    the app is "disabled".

--- TodoAzure/App.cs
+++ TodoAzure/App.cs
@@ -1,5 +1,6 @@
-﻿/*
-   Copyright 2018 Xamarin Inc.
+/*
+   Copyright 2018 PreEmptive Solutions, LLC
+   Portions Copyright 2018 Xamarin Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,6 +15,7 @@
    limitations under the License.
 */
 
+﻿using System;
 using Xamarin.Forms;
 
 namespace TodoAzure
@@ -30,6 +32,20 @@ namespace TodoAzure
         public static void Init(IAuthenticate authenticator)
         {
             Authenticator = authenticator;
+        }
+
+        private const string DisabledPropertyKey = "AppStatus";
+        public static void DisableIfCompromised(bool wasCompromised)
+        {
+            if (!wasCompromised) { return; }
+            Current.Properties[DisabledPropertyKey] = new Random().Next();
+            SavePropertiesNow();
+        }
+        public static bool IsDisabled => Current.Properties.ContainsKey(DisabledPropertyKey);
+
+        private static void SavePropertiesNow()
+        {
+            Current.SavePropertiesAsync().Wait(2000);
         }
 
         protected override void OnStart()
